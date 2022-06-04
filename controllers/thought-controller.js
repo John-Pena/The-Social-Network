0f3a0b305592508854ba8e2f476eb8c2832f0469
John-Pena <jmp1495@gmail.com>
2022-06-04 14:29:37 -0500

    added a reaction to allow users to react to other thoughts

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -23,6 +23,23 @@ const thoughtController = {
       .catch(err => res.json(err));
   },
 
+  // add a reaction
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then(userData => {
+        if (!userData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(userData);
+      })
+      .catch(err => res.json(err));
+  },
+
   // remove Thought
 };
 
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 const {
-  addThought
+  addThought,
+  addReaction
 } = require('../../controllers/thought-controller');
 
 // /api/thoughts/<userId>
 router.route('/:userId').post(addThought)
 
 // /api/thoughts/<userId>/<thoughtId>
+router
+  .route('/:userId/:thoughtId')
+  .put(addReaction)
+;
 
 module.exports = router;
