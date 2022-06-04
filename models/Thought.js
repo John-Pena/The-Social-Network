9105e3b7d292f6135999da215b64f3d43928ecdb
John-Pena <jmp1495@gmail.com>
2022-06-04 13:56:46 -0500

    ReactionSchema set up and tied to other models

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,27 @@
 const { Schema, model, Types } = require('mongoose');
 
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    }
+  }
+);
+
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
@@ -14,9 +36,20 @@ const ThoughtSchema = new Schema(
     username: {
       type: String,
       required: true
-    }
+    },
+    reactions: [ReactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    },
+    id: false
   }
 );
+
+ThoughtSchema.virtuals('reactionCount').get(function() {
+  return this.reactions.length;
+});
 
 const Thought = model('Thought', ThoughtSchema);
 
--- models/User.js
+++ models/User.js
@@ -15,9 +15,28 @@ const UserSchema = new Schema(
       required: true,
       unique: true,
       match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
-    }
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    },
+    id: false
   }
 );
+
+UserSchema.virtual('thoughtCount').get(function() {
+  return this.thoughts.reduce(
+    (total, thought) => total + thought.reactions.length + 1,
+    0
+  );
+});
 
 const User = model('User', UserSchema);
 
