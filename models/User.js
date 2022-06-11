4edc311a06bad87d0cb5dbd03d98627033f6db99
John-Pena <jmp1495@gmail.com>
2022-06-11 13:03:08 -0500

    created friend virtual

--- models/User.js
+++ models/User.js
@@ -19,21 +19,25 @@ const UserSchema = new Schema(
         type: Schema.Types.ObjectId,
         ref: 'Thought'
       }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
     ]
   },
   {
     toJSON: {
-      virtuals: true
+      virtuals: true,
+      getters: true
     },
     id: false
   }
 );
 
-UserSchema.virtual('thoughtCount').get(function() {
-  return this.thoughts.reduce(
-    (total, thought) => total + thought.reactions.length + 1,
-    0
-  );
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
 });
 
 const User = model('User', UserSchema);
